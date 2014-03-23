53d3e968b3be189cdb194e0b9eb19339b59a60fd
hugovk <hugovk@users.noreply.github.com>
2014-03-23 10:36:25 +0200

    Refactor, add absolute path for session key, platform-indepedent output, don't scrobble anything over a day old (for when BBC scrobbling breaks), wait 30 not 10 seconds between checks as that's the Last.fm min play time

--- bbcscrobbler.py
+++ bbcscrobbler.py
@@ -1,7 +1,7 @@
 #! /usr/bin/python
 # Author: Amr Hassan <amr.hassan@gmail.com>
 
-# Enable (uncomment) one of these radio_stations
+# Enable (uncomment) one of these radio stations
 STATION = "bbc6music"
 #STATION = "bbcradio1"
 #STATION = "bbcradio2"
@@ -10,63 +10,115 @@ STATION = "bbc6music"
 
 # Do not edit anything below this line
 
-import pylast, os, time
+import os
+import pylast
+from sys import platform as _platform
+import time
 
 API_KEY = "8fe0d07b4879e9cd6f8d78d86a8f447c"
 API_SECRET = "debb11ad5da3be07d06fddd8fe95cc42"
 
+SESSION_KEY_FILE = "/Users/hugo/Dropbox/bin/data/.session_key"
+
 network = pylast.LastFMNetwork(API_KEY, API_SECRET)
 station = network.get_user(STATION)
 
-if not os.path.exists(".session_key"):
+ONE_DAY_IN_SECONDS = 60*60*24
+
+
+class escape (Exception):
+    pass
+
+
+def output(text):
+    print(text)
+    # Windows:
+    if _platform == "win32":
+        if "&" in text:
+            text.replace("&", "^&")  # escape ampersand
+        os.system("title " + text)
+    # Linux, OS X or Cygwin:
+    elif _platform in ["linux", "linux2", "darwin", "cygwin"]:
+        import sys
+        sys.stdout.write("\x1b]2;" + text + "\x07")
+
+
+def duration(track):
+    return int(track.track.get_duration())/1000
+
+if not os.path.exists(SESSION_KEY_FILE):
     skg = pylast.SessionKeyGenerator(network)
     url = skg.get_web_auth_url()
-    
-    print("Please authorize the scrobbler to scrobble to your account: %s\n" %url)
-    
+
+    print(
+        "Please authorize the scrobbler "
+        "to scrobble to your account: %s\n" % url)
+    import webbrowser
+    webbrowser.open(url)
+
     while True:
         try:
             session_key = skg.get_web_auth_session_key(url)
-            fp = open(".session_key", "w")
+            fp = open(SESSION_KEY_FILE, "w")
             fp.write(session_key)
             fp.close()
             break
         except pylast.WSError:
             time.sleep(1)
 else:
-    session_key = open(".session_key").read()
+    session_key = open(SESSION_KEY_FILE).read()
 
 network.session_key = session_key
 
-print("Tuned in to %s\n----------------------" %STATION)
+output("Tuned in to %s\n---------------------" % STATION)
 
 playing_track = None
 playing_track_scrobbled = False
 while True:
-    
+
     try:
         new_track = station.get_recent_tracks(1)[0]
-    
+
+        if (time.time() - int(new_track.timestamp)) > ONE_DAY_IN_SECONDS:
+            print "Last track over a day ago, don't scrobble"
+            raise escape
+
         if new_track != playing_track:
-            
+
             if playing_track and not playing_track_scrobbled:
-                network.scrobble(playing_track.track.artist.name, playing_track.track.title, playing_track.timestamp)
+                network.scrobble(
+                    playing_track.track.artist.name,
+                    playing_track.track.title,
+                    playing_track.timestamp,
+                    duration=str(duration(playing_track)))
                 playing_track_scrobbled = True
-                print("Scrobbled: %s" %playing_track.track)
-            
-            network.update_now_playing(new_track.track.artist.name, new_track.track.title, duration = str(int(new_track.track.get_duration()/1000)))
-            print("Now playing: %s" %new_track.track)
-            
+                output("Scrobbled: %s" % playing_track.track)
+
+            network.update_now_playing(
+                new_track.track.artist.name,
+                new_track.track.title, duration=str(duration(new_track)))
+            output("Now playing: %s" % new_track.track)
+
             playing_track = new_track
             playing_track_scrobbled = False
-        
-        if playing_track and not playing_track_scrobbled and (time.time() - int(playing_track.timestamp)) >= int(playing_track.track.get_duration())/2000:
-            network.scrobble(playing_track.track.artist.name, playing_track.track.title, playing_track.timestamp)
-            print("Scrobbled: %s" %playing_track.track)
-            playing_track_scrobbled = True
-            
-    except Exception as e:
-        print ("Error: %s" %repr(e))
-    
-    time.sleep(10)
 
+        if playing_track \
+                and not playing_track_scrobbled \
+                and (time.time() - int(playing_track.timestamp)) \
+                >= duration(playing_track)/2:
+            network.scrobble(
+                playing_track.track.artist.name,
+                playing_track.track.title,
+                playing_track.timestamp,
+                duration=str(duration(playing_track)))
+            output("Scrobbled: %s" % playing_track.track)
+            playing_track_scrobbled = True
+
+    except escape:
+        pass
+    except Exception as e:
+        print ("Error: %s" % repr(e))
+
+    time.sleep(30)
+
+# End of file
